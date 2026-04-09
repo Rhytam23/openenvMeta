94d8e5cab6256d84fcfb1e0d2042e57ef2aa709f
Rhytam23 <2020sumoy@gmail.com>
2026-04-09 08:55:22 +0530

    Honor entrypoint args for inference runs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -e
 
+if [ "$#" -gt 0 ]; then
+  exec "$@"
+fi
+
 echo "Starting OpenEnv Smart Parking Server on 0.0.0.0:${PORT:-7860}..."
-python -m server.app
+exec python -m server.app
